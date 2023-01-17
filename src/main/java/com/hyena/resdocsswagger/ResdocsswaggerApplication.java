95796e2355e9f65ca10c76c89a99ea45dd158657
hyunseo <goatsfish@gmail.com>
2023-01-17 20:09:18 +0900

    init : restdocs-swagger

---
+++ src/main/java/com/hyena/resdocsswagger/ResdocsswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.hyena.resdocsswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResdocsswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResdocsswaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyena/resdocsswagger/ResdocsswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyena.resdocsswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResdocsswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
